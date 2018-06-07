883dd3d5ea68ec5d38df568ce642d5d5c8e7af74
bojja-s <bojja.s@hcl.com>
2018-06-07 20:57:46 +0530

    Add files via upload

---
+++ Server/src/test/java/com/hcl/bootcamp/service/HelloWorldServiceTest.java
@@ -0,0 +1,25 @@
+package com.hcl.bootcamp.service;
+
+import com.hcl.bootcamp.service.HelloWorldService;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Bojja
+ */
+public class MessageServiceTest {
+
+    private HelloWorldService m_HelloWorldService;
+
+    @Before
+    public void setUp() {
+        m_HelloWorldService = new HelloWorldService();
+    }
+
+    @Test
+    public void getMessage_ShouldReturnMessage() {
+        assertEquals("Hello World!", m_HelloWorldService.getMessage());
+    }
+}
